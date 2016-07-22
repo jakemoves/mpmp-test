d78ad5c9acde2d15462d600b7045bcf7c321f437
Luke Garwood <luke_gar@hotmail.com>
2016-07-22 03:14:38 +0200

    changed UI on menu to better fit landscape

--- Assets/CustomScripts/connectivityCheck.cs
+++ Assets/CustomScripts/connectivityCheck.cs
@@ -15,6 +15,7 @@ public class connectivityCheck : MonoBehaviour {
 	public Button beginAR;
 
 	IEnumerator Start(){
+		//puts screen in landscape
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
 		//check if we hear anything from Google
 		WWW www = new WWW ("http://google.com");
@@ -44,7 +45,7 @@ public class connectivityCheck : MonoBehaviour {
 	void OnGUI(){ //--all the GUI manipulation has to go in here
 		if(connected == false){
 			GUI.Box(new Rect(0, 0, Screen.width, Screen.height/2), "<size=25><color=white>I'm sorry, your device is not connected to a network</color></size>", style);
-			GUI.Box(new Rect(0, 35, Screen.width, Screen.height/2), "<size=20><color=white>Please force quit this app, check your network settings, and try again</color></size>", style);
+			GUI.Box(new Rect(0, 35, Screen.width, Screen.height/2), "<size=20><color=white>Please quit this app, check your network settings, and try again</color></size>", style);
 		}
 	}
 
